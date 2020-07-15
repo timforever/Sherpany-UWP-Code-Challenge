824197deabcb32f4c11fd2b40122ea602dff092d
Tim Eicher <tim@timeicher.com>
2020-07-15 15:00:42 -0500

    2. Enabled dragging the "Press me" button.
    This is all done in the View logic because it is only concerned with the view and not the view model.

--- Sherpany UWP Code Challenge/View/Pages/MainPage.xaml.cs
+++ Sherpany UWP Code Challenge/View/Pages/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Sherpany_UWP_Code_Challenge.ViewModel;
 using Sherpany_UWP_Code_Challenge.ViewModel.Pages;
 
@@ -18,5 +19,26 @@ namespace Sherpany_UWP_Code_Challenge
         }
 
         public MainPageViewModel VM { get; }
+
+        private void OnManipulationStarted(object sender, Windows.UI.Xaml.Input.ManipulationStartedRoutedEventArgs e)
+        {
+            Button.IsEnabled = false;
+            e.Handled = true;
+        }
+
+        private void OnManipulationDelta(object sender, Windows.UI.Xaml.Input.ManipulationDeltaRoutedEventArgs e)
+        {
+            var transform = DragableGrid.RenderTransform as CompositeTransform;
+            transform.TranslateX += e.Delta.Translation.X;
+            transform.TranslateY += e.Delta.Translation.Y;
+
+            e.Handled = true;
+        }
+
+        private void OnManipulationCompleted(object sender, Windows.UI.Xaml.Input.ManipulationCompletedRoutedEventArgs e)
+        {
+            Button.IsEnabled = true;
+            e.Handled = true;
+        }
     }
 }
