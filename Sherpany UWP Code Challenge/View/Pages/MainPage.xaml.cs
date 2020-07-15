701fc39775c7dcfa87710a64c48f889389c86b0a
Tim Eicher <tim@timeicher.com>
2020-07-15 13:48:13 -0500

    1. Refactored MainPageView and MainPageViewModel so that they more closely follow the MVVM pattern.

--- Sherpany UWP Code Challenge/View/Pages/MainPage.xaml.cs
+++ Sherpany UWP Code Challenge/View/Pages/MainPage.xaml.cs
@@ -1,41 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using GalaSoft.MvvmLight.Messaging;
-using Sherpany_UWP_Code_Challenge.Messages;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Sherpany_UWP_Code_Challenge.ViewModel;
+using Sherpany_UWP_Code_Challenge.ViewModel.Pages;
 
 namespace Sherpany_UWP_Code_Challenge
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// The Main Page of the application.
     /// </summary>
     public sealed partial class MainPageView : Page
     {
         public MainPageView()
         {
-            this.InitializeComponent();
-
+            InitializeComponent();
+            var viewModelLocator = (ViewModelLocator) Application.Current.Resources["Locator"];
+            VM = viewModelLocator.MainPage;
         }
 
-        private async void ButtonTapped(object sender, TappedRoutedEventArgs e)
-        {
-            CloseAppAnimation.Begin();
-            await Task.Delay(2000);
-            Messenger.Default.Send(new CloseAppMessage());
-        }
+        public MainPageViewModel VM { get; }
     }
 }
--- Sherpany UWP Code Challenge/ViewModel/Pages/MainPageViewModel.cs
+++ Sherpany UWP Code Challenge/ViewModel/Pages/MainPageViewModel.cs
@@ -1,26 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
 using GalaSoft.MvvmLight.Views;
+using Sherpany_UWP_Code_Challenge.Messages;
 
 namespace Sherpany_UWP_Code_Challenge.ViewModel.Pages
 {
     public class MainPageViewModel: ViewModelBase
     {
         private readonly INavigationService _navigationService;
+        
+        private bool _isAppClosing = false;
+
+        public bool IsAppClosing
+        {
+            get => _isAppClosing;
+            private set => Set(ref _isAppClosing, value);
+        }
 
         public MainPageViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
         }
 
+        /// <summary>
+        /// This method will kick off the closing of the app.
+        /// </summary>
+        public async void StartAppClose()
+        {
+            // Don't trigger this more than once.
+            if (IsAppClosing)
+                return;
 
-        
+            IsAppClosing = true;
+            await Task.Delay(2000);
+            Messenger.Default.Send(new CloseAppMessage());
+        }
+
         //TODO If no passcode is set in the vault, the user can enter one and will then be navigated to the DetailPageView
         public ICommand SetPasswordAndNavigateCommand => new RelayCommand<string>(SetPasswordAndNavigate);
 
